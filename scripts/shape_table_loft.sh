7e70ff2652c1b945754edae5f5b634f20236f3a2
BunnySoCrazy <Master_Origami@163.com>
2025-10-07 17:23:33 +0800

    Initial commit

--- scripts/shape_table_loft.sh
+++ scripts/shape_table_loft.sh
@@ -1,6 +1,6 @@
-python train.py -e shape_table_loft --grid 16  --epoch 1 -g 5
-python train.py -e shape_table_loft --continue --grid 32 --ckpt best_16  --epoch 1  -g 5
-python train.py -e shape_table_loft --continue --grid 64 --ckpt best_32  --epoch 1 -g 5
+python train.py -e shape_table_loft --grid 16  --epoch 20 -g 5
+python train.py -e shape_table_loft --continue --grid 32 --ckpt best_16  --epoch 20  -g 5
+python train.py -e shape_table_loft --continue --grid 64 --ckpt best_32  --epoch 30 -g 5
 
 python fine-tuning.py -e shape_table_loft -g 5 --start 0 --end 2 --epoches 10 --grid 16 -c best_64  --test_data 
 python fine-tuning.py -e shape_table_loft -g 5 --start 0 --end 2 --epoches 50 --grid 32 -c best_16 -l  --test_data 
